0aee2f8d95ffbf57d57cd55a3afa6be733991382
HugoLaurent <lauhugopro@gmail.com>
2024-08-05 11:00:51 +0200

    add: dark mode switch button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+import { Colors } from "@/constants/Colors"; // Assurez-vous que ce fichier existe et contient les styles nécessaires
 
 export default function Index() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        {
+          backgroundColor: isDarkMode
+            ? Colors.dark.background
+            : Colors.light.background,
+        },
+      ]}
     >
-      <Text>Edit app/index.tsx to celui qsdqsd? this screen.</Text>
+      <Text
+        style={{ color: isDarkMode ? Colors.dark.text : Colors.light.text }}
+      >
+        Edit app/index.tsx to celui qsdqsd? this screen.
+      </Text>
+      <Button title="Toggle Dark Mode" onPress={toggleDarkMode} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
