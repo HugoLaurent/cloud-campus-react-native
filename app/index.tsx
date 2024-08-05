680055297555f18fc0b893612e25b19f66f39fcc
HugoLaurent <lauhugopro@gmail.com>
2024-08-05 11:16:26 +0200

    Step Box

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,13 @@
+import style from "@/style";
 import React, { useState } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
-import { Colors } from "@/constants/Colors"; // Assurez-vous que ce fichier existe et contient les styles nécessaires
 
 export default function Index() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
-
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: isDarkMode
-            ? Colors.dark.background
-            : Colors.light.background,
-        },
-      ]}
-    >
-      <Text
-        style={{ color: isDarkMode ? Colors.dark.text : Colors.light.text }}
-      >
-        Edit app/index.tsx to celui qsdqsd? this screen.
-      </Text>
-      <Button title="Toggle Dark Mode" onPress={toggleDarkMode} />
+    <View style={style.container}>
+      <View style={style.box}>
+        <Text style={style.boxText}>Hello, world!</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
